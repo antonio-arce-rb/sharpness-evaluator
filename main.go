b7fde2c80d3e0db15636e1114832dba38cdfedf0
lilianamolloy <39354383+lilianamolloy@users.noreply.github.com>
2024-07-19 14:55:01 +1000

    yolo image extraction

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/jpeg"
 	"os"
 )
 
 // PSEUDOCODE
-// what is image data
-// extract image data in go
+// what is image data ✅
+// extract image data in go ✅
 // add the package "blurry"
 // figure out what image data "blurry" requires
 // add gaussian-blur to image
@@ -16,27 +17,44 @@ import (
 // apply laplacian matrix
 // convolute and return laplacian variance
 
-const TestFilepath = "images/best-seller-poster/13060191.jpg"
+const TestFilepath string = "images/best-seller-poster/13060191.jpg"
 
 func main() {
-	_, err := decodeImage(TestFilepath)
+	myImage, err := decodeImage(TestFilepath)
 	if err != nil {
+		fmt.Println("Dis not working")
 		fmt.Println(err)
+		return
 	}
 	fmt.Println("decoded the image")
+	fmt.Println(myImage)
 }
 
 func decodeImage(filePath string) (image.Image, error) {
-	file, err := os.Open(filePath)
+	existingImageFile, err := os.Open(filePath)
 	if err != nil {
-		return nil, err
+		fmt.Println("Error opening files")
 	}
-	defer file.Close()
+	defer existingImageFile.Close()
 
-	img, _, err := image.Decode(file)
+	_, imageType, err := image.Decode(existingImageFile)
 	if err != nil {
-		return nil, err
+		fmt.Println("Error decoding image")
+		fmt.Println(err)
 	}
 
-	return img, nil
+	// We only need this because we already read from the file
+	// We have to reset the file pointer back to beginning
+	existingImageFile.Seek(0, 0)
+
+	if imageType == "jpeg" {
+		loadedImage, err := jpeg.Decode(existingImageFile)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(loadedImage)
+		return loadedImage, nil
+	}
+
+	return nil, fmt.Errorf("image type not supported")
 }
