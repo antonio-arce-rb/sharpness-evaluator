b9a093a0f95a506d5091774e659e5f0761aa590b
antonio-arce-rb <antonio.arce@redbubble.com>
2024-07-19 13:48:36 +1000

    - testing image processing

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+	"os"
+)
 
-func main() {
-	fmt.Println("Hello, World!")
-}
-
+// PSEUDOCODE
 // what is image data
 // extract image data in go
 // add the package "blurry"
@@ -14,3 +15,28 @@ func main() {
 // make image greyscale
 // apply laplacian matrix
 // convolute and return laplacian variance
+
+const TestFilepath = "images/best-seller-poster/13060191.jpg"
+
+func main() {
+	_, err := decodeImage(TestFilepath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("decoded the image")
+}
+
+func decodeImage(filePath string) (image.Image, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
