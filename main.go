b677f91bf6bd322b650c81d417154413d7d40d59
antonio-arce-rb <antonio.arce@redbubble.com>
2024-07-19 12:12:10 +1000

    - pseudocode

--- main.go
+++ main.go
@@ -5,3 +5,12 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+// what is image data
+// extract image data in go
+// add the package "blurry"
+// figure out what image data "blurry" requires
+// add gaussian-blur to image
+// make image greyscale
+// apply laplacian matrix
+// convolute and return laplacian variance
